d2c50935ec523df281dfd5fe0f13b9115a6b5dfe
danielmf <danielmf@gmail.com>
2014-06-12 13:56:26 +0200

    Fix code style

--- lib/Digitick/Sepa/DomBuilder/CustomerDirectDebitTransferDomBuilder.php
+++ lib/Digitick/Sepa/DomBuilder/CustomerDirectDebitTransferDomBuilder.php
@@ -175,31 +175,30 @@ class CustomerDirectDebitTransferDomBuilder extends BaseDomBuilder
 
     }
 
+    /**
+     * Add the specific OrgId element for the format 'pain.008.001.02'
+     *
+     * @param  GroupHeader $groupHeader
+     * @return mixed
+     */
+    public function visitGroupHeader(GroupHeader $groupHeader)
+    {
+        parent::visitGroupHeader($groupHeader);
 
-	/**
-	 * Add the specific OrgId element for the format 'pain.008.001.02'
-	 *
-	 * @param  GroupHeader $groupHeader
-	 * @return mixed
-	 */
-	public function visitGroupHeader(GroupHeader $groupHeader)
-	{
-		parent::visitGroupHeader($groupHeader);
+        if ($groupHeader->getInitiatingPartyId() !== null && $this->painFormat === 'pain.008.001.02') {
+            $newId = $this->createElement('Id');
+            $orgId = $this->createElement('OrgId');
+            $othr  = $this->createElement('Othr');
+            $othr->appendChild($this->createElement('Id', $groupHeader->getInitiatingPartyId()));
+            $orgId->appendChild($othr);
+            $newId->appendChild($orgId);
 
-		if ($groupHeader->getInitiatingPartyId() !== null && $this->painFormat === 'pain.008.001.02') {
-			$newId = $this->createElement('Id');
-			$orgId = $this->createElement('OrgId');
-			$othr  = $this->createElement('Othr');
-			$othr->appendChild($this->createElement('Id', $groupHeader->getInitiatingPartyId()));
-			$orgId->appendChild($othr);
-			$newId->appendChild($orgId);
+            $xpath = new \DOMXpath($this->doc);
+            $items = $xpath->query('GrpHdr/InitgPty/Id', $this->currentTransfer);
+            $oldId = $items->item(0);
 
-			$xpath = new \DOMXpath($this->doc);
-			$items = $xpath->query('GrpHdr/InitgPty/Id', $this->currentTransfer);
-			$oldId = $items->item(0);
-
-			$oldId->parentNode->replaceChild($newId, $oldId);
-		}
-	}
+            $oldId->parentNode->replaceChild($newId, $oldId);
+        }
+    }
 
 }
